8d7b09a2a44c88ed62342764947ca9421bb063f3
jeffngugi <ngugimwathi254@gmail.com>
2023-02-09 11:13:36 +0300

    chore(ios):Setup appcenter for ios

--- ios/AppCenterRN/AppDelegate.mm
+++ ios/AppCenterRN/AppDelegate.mm
@@ -1,6 +1,9 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 @implementation AppDelegate
 
@@ -10,6 +13,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
