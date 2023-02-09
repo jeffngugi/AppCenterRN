a84aee4daee21931ff6e126a88995ac861ad95ed
jeffngugi <ngugimwathi254@gmail.com>
2023-02-09 14:51:16 +0300

    chore:Add analytics and crash alert

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
-import {Text, Button, SafeAreaView} from 'react-native';
-import React from 'react';
+import {Text, Button, SafeAreaView, Alert} from 'react-native';
+import React, {useEffect} from 'react';
 import Crashes from 'appcenter-crashes';
-
+import Analytics from 'appcenter-analytics';
 const App = () => {
+  const checkPreviousSession = async () => {
+    const didCrash = await Crashes.hasCrashedInLastSession();
+
+    if (didCrash) {
+      Alert.alert('Sorry about the crash, we are working for a solution');
+    }
+  };
+
+  useEffect(() => {
+    checkPreviousSession();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <Text>App</Text>
       <Button
-        title="Crash the app"
-        onPress={() => Crashes.generateTestCrash()}
+        title="Calculate inflation"
+        onPress={() =>
+          Analytics.trackEvent('Calculate_inflation', {wifi: 'off', GPS: 'On'})
+        }
       />
     </SafeAreaView>
   );
